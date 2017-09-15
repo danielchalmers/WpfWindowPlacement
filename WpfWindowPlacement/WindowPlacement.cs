e361e0ab1ccd1a3d9319a458c0ef582e6e595b46
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-09-15 16:56:47 -0400

    Add basic API

---
+++ WpfWindowPlacement/WindowPlacement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WpfWindowPlacement
+{
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WindowPlacement
+    {
+        public int length;
+        public int flags;
+        public int showCmd;
+        public Point minPosition;
+        public Point maxPosition;
+        public Rect normalPosition;
+    }
+}
