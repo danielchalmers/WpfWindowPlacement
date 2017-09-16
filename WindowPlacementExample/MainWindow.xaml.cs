7120fef875fc5c62ad97aaeec75b487f5934f873
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-09-15 20:26:44 -0400

    Add example app

---
+++ WindowPlacementExample/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using WindowPlacementExample.Properties;
+
+namespace WindowPlacementExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Settings.Default.Save();
+        }
+
+        private void ManualSaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.MainWindowPlacement = WpfWindowPlacement.WindowPlacementFunctions.GetPlacement(this);
+            Settings.Default.Save();
+        }
+
+        private void ManualLoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            WpfWindowPlacement.WindowPlacementFunctions.SetPlacement(this, Settings.Default.MainWindowPlacement);
+        }
+    }
+}
